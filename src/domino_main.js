610d66bb789d7e3b6803f2d101ec6bfa42f070d3
Péter Kovács <peter.kovacs0519@gmail.com>
2020-05-05 18:19:10 +0200

    LFLT-412 Package executable of Domino and create a CircleCI build plan for it  - Replaced di_config.yml with di_config.json to support packaging  - Added support scripts for packaging  - Started configuring CircleCI build plan

--- src/domino_main.js
+++ src/domino_main.js
@@ -1,10 +1,10 @@
-import {ContainerBuilder, YamlFileLoader} from "node-dependency-injection";
+import {ContainerBuilder, JsonFileLoader} from "node-dependency-injection";
 import path from "path";
 
 // create IoC container
 const container = new ContainerBuilder(true, path.join(__dirname));
-const loader = new YamlFileLoader(container);
-loader.load("./config/di_config.yml");
+const loader = new JsonFileLoader(container);
+loader.load(path.join(__dirname, "config/di_config.json"));
 container.compile();
 
 // start app
