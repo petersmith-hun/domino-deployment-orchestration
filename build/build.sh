cd50c32b74886342bc915ab175109efb06c7e5be
Péter Kovács <peter.kovacs0519@gmail.com>
2020-05-05 19:04:20 +0200

    LFLT-412 Package executable of Domino and create a CircleCI build plan for it  - Fixed installing pkg

--- build/build.sh
+++ build/build.sh
@@ -3,13 +3,13 @@
 npm install
 npm install @babel/cli
 npm install @babel/core
-npm install -g pkg
+npm install pkg
 npx babel src -d build/target/for_packaging/es5_src
 cp package.json build/target/for_packaging/package.json
 mkdir build/target/for_packaging/es5_src/config
 cp config/di_config.json build/target/for_packaging/es5_src/config/di_config.json
 node build/generate_require_file.js
-pkg --targets=linux --output=build/target/out/domino .
+npx pkg --targets=linux --output=build/target/out/domino .
 mkdir build/target/out/logs
 cp node_modules/bcrypt/build/Release/bcrypt_lib.node build/target/out/bcrypt_lib.node
 cp node_modules/process-list/build/Release/processlist.node build/target/out/processlist.node
